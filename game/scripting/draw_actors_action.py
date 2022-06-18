f2353bb6b1a765555e9758a204c582ed0a4ce2a7
broomfactory <104051666+broomfactory@users.noreply.github.com>
2022-06-18 12:52:10 -0600

    Updated to remove food, snake and add players

--- game/scripting/draw_actors_action.py
+++ game/scripting/draw_actors_action.py
@@ -27,14 +27,15 @@ class DrawActorsAction(Action):
             script (Script): The script of Actions in the game.
         """
         score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
-        snake = cast.get_first_actor("snakes")
-        segments = snake.get_segments()
+        player1 = cast.get_first_actor("player1")
+        player2 = cast.get_first_actor("player2")
+        trail1 = player1.get_segments()
+        trail2 = player1.get_segments()
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actor(food)
-        self._video_service.draw_actors(segments)
+        self._video_service.draw_actor(trail1)
+        self._video_service.draw_actors(trail2)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
